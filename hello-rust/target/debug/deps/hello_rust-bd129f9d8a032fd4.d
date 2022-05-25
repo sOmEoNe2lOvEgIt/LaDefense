dd8da2fadf9913a1f57ff040067569bcccaa075b
sOmEoNe2lOvEgIt <raymcraft@gmail.com>
2022-05-26 01:34:04 +0200

    Added cargo new hello world folder

---
+++ hello-rust/target/debug/deps/hello_rust-bd129f9d8a032fd4.d
@@ -0,0 +1,5 @@
+/home/someone2love/PersonalProjects/LaDefense/hello-rust/target/debug/deps/hello_rust-bd129f9d8a032fd4: src/main.rs
+
+/home/someone2love/PersonalProjects/LaDefense/hello-rust/target/debug/deps/hello_rust-bd129f9d8a032fd4.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/someone2love/PersonalProjects/LaDefense/hello-rust/target/debug/hello-rust: /home/someone2love/PersonalProjects/LaDefense/hello-rust/src/main.rs
