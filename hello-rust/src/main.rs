809aac640d4dee89fb5d3c3056710e8b31b26bea
sOmEoNe2lOvEgIt <raymcraft@gmail.com>
2022-05-26 02:12:51 +0200

    Added gitignore, onto removing debug files etc

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
+    let hello = String::from("hello");
+    println!("{}", hello);
 }
---
+++ hello-rust/target/debug/deps/hello_rust-40e50108da6b9282.d
@@ -0,0 +1,5 @@
+/home/someone2love/PersonalProjects/LaDefense/hello-rust/target/debug/deps/hello_rust-40e50108da6b9282: src/main.rs
+
+/home/someone2love/PersonalProjects/LaDefense/hello-rust/target/debug/deps/hello_rust-40e50108da6b9282.d: src/main.rs
+
+src/main.rs:
